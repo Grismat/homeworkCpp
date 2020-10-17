d90532cba4996a04c1b6d0d3b0769edd703d39f9
Grismat <smirnalexandr@rambler.ru>
2020-10-17 18:32:49 +0300

    Task 2.3 solved

---
+++ homework_2/task-2.3/main.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <stdio.h>
+
+void bubbleSort(int* const begin, int* const end)
+{
+    size_t size = end - begin;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 1; j < size - i; j++)
+        {
+            if (begin[j] < begin[j-1])
+            {
+                std::swap(begin[j], begin[j-1]);
+            }
+        }
+    }
+}
+
+void countingSort(int* const begin, int* const  end)
+{
+    size_t size = end - begin;
+
+    int maxElement = begin[0];
+    for (size_t i = 1; i < size; i++)
+    {
+        if (begin[i] > maxElement)
+        {
+            maxElement = begin[i];
+        }
+    }
+
+    int* valuesCount = new int[maxElement+1]();
+    for (size_t i = 0; i < size; i++)
+    {
+        valuesCount[begin[i]]++;
+    }
+
+    for (size_t i = 0; i <= maxElement; i++)
+    {
+        int mainIdx = 0;
+        while (valuesCount[i] > 0) {
+            begin[mainIdx] = valuesCount[i]--;
+            mainIdx++;
+        }
+    }
+}
+
+int main()
+{
+    printf("Here's the simple test for two sort functions\n");
+
+    int array0[14] = {4, 1, 6, 3, 7, 2, 3, 6, 3, 8, 1, 1, 1, 43};
+    int array1[14] = {4, 1, 6, 3, 7, 2, 3, 6, 3, 8, 1, 1, 1, 43};
+    int array2[14] = {4, 1, 6, 3, 7, 2, 3, 6, 3, 8, 1, 1, 1, 43};
+
+    std::sort(array0, array0+14);
+    bubbleSort(array1, array1+14);
+    countingSort(array2, array2+14);
+
+    printf("Standart sort function:\n\t");
+    for (int i = 0; i < 13; i++)
+    {
+        printf("%i ", array1[i]);
+    }
+    printf("\n");
+
+    printf("BubbleSort:\n\t");
+    for (int i = 0; i < 13; i++)
+    {
+        printf("%i ", array1[i]);
+    }
+    printf("\n");
+
+    printf("CountingSort:\n\t");
+    for (int i = 0; i < 13; i++)
+    {
+        printf("%i ", array1[i]);
+    }
+
+    return 0;
+}
