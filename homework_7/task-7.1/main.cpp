2b3c554203ac042e7f5eae001c6975a81bdd38a3
Grismat <smirnalexandr@rambler.ru>
2020-11-08 18:42:43 +0300

    Task-7.1 solved

---
+++ homework_7/task-7.1/main.cpp
@@ -0,0 +1,40 @@
+#include "SortedList.h"
+#include <stdio.h>
+
+int main() {
+
+    char command[2];
+    int value = 0;
+    SortedList* list = new SortedList;
+    
+    do {
+        fgets(command, 2, stdin);
+
+        switch (command[0]) {
+        case '1':
+            printf("Enter a number you want to add\n");
+            scanf("%i", &value);
+            addElement(list, value);
+            break;
+        
+        case '2':
+            printf("Enter a number you want to remove\n");
+            scanf("%i", &value);
+            if (removeElement(list, value)) {
+                printf("Removed successfully\n");
+            }
+            else {
+                printf("Removing failed\n");
+            }
+            break;
+
+        case '3':
+            printf("Here's the list:\n");
+            printList(list);
+            break;
+        }
+
+    } while (command[0] != '0');
+
+    return 0;
+}
